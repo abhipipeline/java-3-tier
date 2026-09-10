b49740f84b25a674648f9808722d07e5ff1130e5
abhipipeline <b21cs042@kitsw.ac.in>
2026-09-10 20:16:38 +0530

    Convert ASG to GCP Instance Group and Instance Template

--- infrastructure/modules/asg/main.tf
+++ infrastructure/modules/asg/main.tf
@@ -1,61 +1,102 @@
-# ASG Module
+# GCP Instance Group Module
 
-resource "aws_launch_template" "main" {
-  name_prefix   = "${var.environment}-lt"
-  image_id      = "ami-0c02fb55956c7d316" # Amazon Linux 2
-  instance_type = var.instance_type
-  key_name      = var.key_name
+# Instance Template
+resource "google_compute_instance_template" "main" {
+  name          = "${var.environment}-instance-template"
+  machine_type  = var.instance_type
+  region        = var.region
+  can_ip_forward = false
 
-  vpc_security_group_ids = var.security_group_ids
-
-  user_data = base64encode(<<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y java-11-amazon-corretto
-              yum install -y tomcat
-              systemctl enable tomcat
-              systemctl start tomcat
-              EOF
-  )
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name        = "${var.environment}-web-instance"
-      Environment = var.environment
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      size  = 20
     }
   }
+
+  network_interface {
+    network            = var.network_name
+    subnetwork         = var.subnet_name
+    network_ip         = ""
+    access_config {
+      nat_ip = ""
+    }
+  }
+
+  service_account {
+    email  = var.service_account_email
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
+
+  metadata = {
+    startup-script = base64encode(<<-EOF
+                      #!/bin/bash
+                      apt-get update
+                      apt-get install -y default-jdk
+                      apt-get install -y tomcat10
+                      systemctl enable tomcat10
+                      systemctl start tomcat10
+                      EOF
+    )
+  }
+
+  labels = {
+    environment = var.environment
+    name        = "${var.environment}-web-instance"
+  }
+
+  tags = [var.environment, "web"]
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_autoscaling_group" "main" {
-  name                = "${var.environment}-asg"
-  vpc_zone_identifier = var.private_subnet_ids
-  target_group_arns   = var.target_group_arns
-  health_check_type   = "ELB"
-  health_check_grace_period = 300
+# Instance Group Manager (Managed Instance Group)
+resource "google_compute_instance_group_manager" "main" {
+  name               = "${var.environment}-instance-group-manager"
+  base_instance_name = "${var.environment}-web"
+  instance_template  = google_compute_instance_template.main.self_link
+  zone               = var.zone
 
-  min_size         = var.min_size
-  max_size         = var.max_size
-  desired_capacity = var.desired_capacity
+  target_size = var.desired_capacity
 
-  launch_template {
-    id      = aws_launch_template.main.id
-    version = "$Latest"
+  named_port {
+    name = "http"
+    port = 8080
   }
 
-  tag {
-    key                 = "Name"
-    value               = "${var.environment}-web-instance"
-    propagate_at_launch = true
+  auto_healing_policies {
+    health_check      = google_compute_health_check.main.id
+    initial_delay_sec = 300
   }
+}
 
-  tag {
-    key                 = "Environment"
-    value               = var.environment
-    propagate_at_launch = true
+# Autoscaler
+resource "google_compute_autoscaler" "main" {
+  name       = "${var.environment}-autoscaler"
+  zone       = var.zone
+  target     = google_compute_instance_group_manager.main.id
+  depends_on = [google_compute_instance_group_manager.main]
+
+  autoscaling_policy {
+    min_replicas    = var.min_size
+    max_replicas    = var.max_size
+    cooldown_period = 300
+
+    cpu_utilization {
+      target = 0.7
+    }
+  }
+}
+
+# Health Check
+resource "google_compute_health_check" "main" {
+  name        = "${var.environment}-health-check"
+  description = "Health check for application servers"
+
+  http_health_check {
+    port         = 8080
+    request_path = "/"
   }
 }
